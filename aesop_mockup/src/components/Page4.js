3b1d0ae382e8735b3b2749b77fd4d7c5b28388c7
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-15 16:18:48 -0500

    testing

---
+++ aesop_mockup/src/components/Page4.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import Aesop from "../images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg";
+
+function Page4() {
+    return(
+    
+    <div>
+        <img src={Aesop} width={"60%"} height={"60%"}/>
+    </div>    
+    ); 
+    }
+
+
+export default Page4;
