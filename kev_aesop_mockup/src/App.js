84bc940ea281210b9a3523ba7ab5c467a90c2ce0
KevinJ.Regis <dev.regis1@gmail.com>
2020-04-16 02:38:59 -0500

    moved kevBodyCleansingSlab into images folder

--- kev_aesop_mockup/src/App.js
+++ kev_aesop_mockup/src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import './App.css';
 import KevsAesopsGift from './component/images/KevsAesopsGift.png';
-import kev_Body_Cleansing_Slab from './component/kevs_odyssey/kev_Body_Cleansing_Slab';
-import kev_Redemption_BodyScrub from './component/kevs_odyssey/kev_Redemption_BodyScrub';
-import kev_Reverence_Handwash from './component/kevs_odyssey/kev_Reverence_Handwash';
+import kevBodyCleansingSlab from './component/kevs_odyssey/kevBodyCleansingSlab';
+// import kev_Redemption_BodyScrub from './component/kevs_odyssey/kev_Redemption_BodyScrub';
+// import kev_Reverence_Handwash from './component/kevs_odyssey/kev_Reverence_Handwash';
 
 
 class App extends React.Component {
@@ -19,9 +19,9 @@ class App extends React.Component {
         <br/>
         <h4> An odyssey of gifts, for generous and inspired giving</h4>
           <img src={KevsAesopsGift} className="mainfloatImg"alt="ladderimg" />
-          <img src={ kev_Body_Cleansing_Slab } className="cleanseSlabpic"alt="ladderimg" />
-          <img src={ kev_Redemption_BodyScrub } className="bodyscrubpic"alt="ladderimg" />
-          <img src={ kev_Reverence_Handwash } className="handwashpic"alt="ladderimg" />
+          <img src={ kevBodyCleansingSlab } className="cleanseSlabpic"alt="slabimg" />
+          {/* <img src={ kev_Redemption_BodyScrub } className="bodyscrubpic"alt="ladderimg" />
+          <img src={ kev_Reverence_Handwash } className="handwashpic"alt="ladderimg" /> */}
             
             <p> Seasonal Gift Kits </p>
             <p> Aesop favourites  </p>
