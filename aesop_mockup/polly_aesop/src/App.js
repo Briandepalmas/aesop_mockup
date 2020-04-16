cfa1a74362958dbfe014bdf4fa390a99f06bb6b5
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-16 15:14:20 -0500

    subsequent update

--- aesop_mockup/polly_aesop/src/App.js
+++ aesop_mockup/polly_aesop/src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Aesop from "./images/Aesop-Bleecker-Street-Carousel-1-Desktop-2560x1440px.jpg";
+ 
+function Page4() {
+   return(
+   <div class="container">
+     <img src={ Aesop } />
+  
+     <h6 id="architecture">Architecture and Design</h6>
+     <h6 id="stores">Aesop stores are informed by the history, fabric and context of x </h6>
+</div>
+   )
 
-function App() {
-  return (
-    <div className="App">
-      
-     
-       
-    </div>
-  );
 }
 
-export default App;
+export default Page4
