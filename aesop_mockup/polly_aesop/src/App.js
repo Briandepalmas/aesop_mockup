c0b5f0bdcbf110fcb856aabe65bb52e8e3ed80a5
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-16 19:40:45 -0500

    fix labels at the top and bottom of the image

--- aesop_mockup/polly_aesop/src/App.js
+++ aesop_mockup/polly_aesop/src/App.js
@@ -7,13 +7,12 @@ function Page4() {
    <div class="container">
      
   
-     <h6 id="architecture">Architecture and Design</h6>
-     <h6 id="stores">Aesop stores are informed by the history, fabric and context of their own environmnets, yet retain an aesthetic consistency. </h6>
+     <div><h6 id="architecture">Architecture and Design &nbsp; Aesop stores are informed by the history, fabric and context of their own environmnets, yet retain an aesthetic consistency. </h6></div>
+     
      <h4 id="spaces">Spaces of distinction</h4>
-     <h6> Find a nearby store -></h6>
+     <h6 id="nearby"> Find a nearby store -></h6>
      <img src={ Aesop } className="fountainImg"alt="waterFountain" />
-     <h6 id="page">1 of 5</h6>
-     <h6 id="address">Aesop Bleecker Street</h6>
+     <div><h6 id="page"> 1 of 5   &nbsp;  Aesop Bleecker Street</h6></div>
      <h6 id="zip">341 Bleecker St, New York, NY 10014</h6>
 
 
