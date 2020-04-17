4640b24136effbb584bf6eda83d012fb5e8b5504
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-17 08:58:46 -0500

    enlarge font size on second line Spaces of distinction

--- aesop_mockup/polly_aesop/src/App.js
+++ aesop_mockup/polly_aesop/src/App.js
@@ -9,11 +9,11 @@ function Page4() {
   
      <div><h6 id="architecture">Architecture and Design &nbsp; Aesop stores are informed by the history, fabric and context of their own environmnets, yet retain an aesthetic consistency. </h6></div>
      
-     <h4 id="spaces">Spaces of distinction</h4>
-     <h6 id="nearby"> Find a nearby store -></h6>
+     <div><h4 id="spaces">Spaces of distinction</h4></div>
+     <div><h6 id="nearby"> Find a nearby store -></h6></div>
      <img src={ Aesop } className="fountainImg"alt="waterFountain" />
      <div><h6 id="page"> 1 of 5   &nbsp;  Aesop Bleecker Street</h6></div>
-     <h6 id="zip">341 Bleecker St, New York, NY 10014</h6>
+     <div><h6 id="zip">341 Bleecker St, New York, NY 1001</h6></div>
 
 
 </div>
